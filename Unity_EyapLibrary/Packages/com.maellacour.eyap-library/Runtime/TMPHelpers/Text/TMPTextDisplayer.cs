5446f6bf9f3511d3313cdfaa7e30605ccc53e949
Mael Lacour <public.dev@maellacour.com>
2024-09-11 12:04:43 +0200

    feat: Add TMPTextDisplayer and SmartDisplayer classes for displaying text easily in Unity
    Can be used with many different types of variables.

---
+++ Unity_EyapLibrary/Packages/com.maellacour.eyap-library/Runtime/TMPHelpers/Text/TMPTextDisplayer.cs
@@ -0,0 +1,44 @@
+namespace EyapLibrary.TMPHelpers.Text
+{
+	using TMPro;
+	using UnityEngine;
+
+	[RequireComponent(typeof(TextMeshProUGUI))]
+	public class TMPTextDisplayer : TMPTextDisplayerBase
+	{
+		public override void UpdateTextField(string value)
+		{
+			_tmpText?.SetText(value);
+		}
+
+		public override void UpdateTextField(int value)
+		{
+			_tmpText?.SetText(value.ToString());
+		}
+
+		public override void UpdateTextField(float value)
+		{
+			_tmpText?.SetText(value.ToString());
+		}
+
+		public override void UpdateTextField(bool value)
+		{
+			_tmpText?.SetText(value.ToString());
+		}
+
+		public override void UpdateTextField(Vector3 value)
+		{
+			_tmpText?.SetText(value.ToString());
+		}
+
+		public override void UpdateTextField(Vector2 value)
+		{
+			_tmpText?.SetText(value.ToString());
+		}
+
+		public override void UpdateTextField(object value)
+		{
+			_tmpText?.SetText(value.ToString());
+		}
+	}
+}
---
+++ Unity_EyapLibrary/Packages/com.maellacour.eyap-library/Runtime/TMPHelpers/Text/TMPTextDisplayerBase.cs
@@ -0,0 +1,31 @@
+namespace EyapLibrary.TMPHelpers.Text
+{
+	using TMPro;
+	using UnityEngine;
+
+	[RequireComponent(typeof(TextMeshProUGUI))]
+	public abstract class TMPTextDisplayerBase : MonoBehaviour
+	{
+		[SerializeField] protected TextMeshProUGUI _tmpText;
+
+		protected virtual void Awake()
+		{
+			if (_tmpText == null)
+			{
+				_tmpText = GetComponent<TextMeshProUGUI>();
+			}
+		}
+
+		public abstract void UpdateTextField(string value);
+
+		public abstract void UpdateTextField(int value);
+
+		public abstract void UpdateTextField(float value);
+
+		public abstract void UpdateTextField(bool value);
+
+		public abstract void UpdateTextField(Vector3 value);
+		public abstract void UpdateTextField(Vector2 value);
+		public abstract void UpdateTextField(object value);
+	}
+}
