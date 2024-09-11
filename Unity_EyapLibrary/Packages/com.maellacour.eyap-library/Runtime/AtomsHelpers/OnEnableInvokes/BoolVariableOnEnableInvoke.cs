888d922dab009d73f3cd9b41d4d8553a3028be8e
Mael Lacour <public.dev@maellacour.com>
2024-09-11 12:04:11 +0200

    feat: Add AtomVariableOnEnableInvoke classes for different variable types

---
+++ Unity_EyapLibrary/Packages/com.maellacour.eyap-library/Runtime/AtomsHelpers/OnEnableInvokes/BoolVariableOnEnableInvoke.cs
@@ -0,0 +1,8 @@
+namespace EyapLibrary.AtomsHelpers.OnEnableInvokes
+{
+	using UnityAtoms.BaseAtoms;
+	using UnityEngine.Events;
+
+	public sealed class BoolVariableOnEnableInvoke : AtomVariableOnEnableInvoke<bool, BoolVariable, UnityEvent<bool>>
+	{ }
+}
---
+++ Unity_EyapLibrary/Packages/com.maellacour.eyap-library/Runtime/AtomsHelpers/OnEnableInvokes/FloatVariableOnEnableInvoke.cs
@@ -0,0 +1,8 @@
+namespace EyapLibrary.AtomsHelpers.OnEnableInvokes
+{
+	using UnityAtoms.BaseAtoms;
+	using UnityEngine.Events;
+
+	public sealed class FloatVariableOnEnableInvoke : AtomVariableOnEnableInvoke<float, FloatVariable, UnityEvent<float>>
+	{ }
+}
